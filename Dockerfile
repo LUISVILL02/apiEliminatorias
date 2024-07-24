5008066a2c5c5b44c7b088e975cc324043c3f3b9
luisvill02 <lvillamil@unimagdalena.edu.co>
2024-07-24 16:25:56 -0500

    update cors configuration

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /app
 
 COPY target/apiEliminatorias-0.0.1-SNAPSHOT.jar /app/apiEliminatorias.jar
 
-EXPOSE 8083
+EXPOSE 0
 
 CMD ["java", "-jar", "/app/apiEliminatorias.jar"]
--- src/main/java/com/eliminatorias/apieliminatorias/security/config/WebConfig.java
+++ src/main/java/com/eliminatorias/apieliminatorias/security/config/WebConfig.java
@@ -24,6 +24,7 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://appeliminatoria.netlify.app");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
